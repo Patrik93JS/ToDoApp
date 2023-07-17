9ac02a92f06fba9ed30fb56317c879a09f34121f
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-07-17 20:44:16 +0200

    complete style

--- FE/todo-app/src/components/todo/Complete.tsx
+++ FE/todo-app/src/components/todo/Complete.tsx
@@ -36,7 +36,15 @@ export const Complete: FC<Props> = ({ todo }) => {
 
   return (
     <div className={styles.completeCheckbox}>
-      <input type="checkbox" onChange={handleChange} checked={todo.attributes.completed} />
+      <input
+        type="checkbox"
+        onChange={handleChange}
+        checked={todo.attributes.completed}
+        className="peer relative appearance-none shrink-0 w-4 h-4 border-2 border-blue-200 rounded-sm mt-1 bg-white
+        focus:outline-none focus:ring-offset-0 focus:ring-1 focus:ring-blue-100
+        checked:bg-green-500 checked:border-0
+        disabled:border-steel-400 disabled:bg-steel-400"
+      />
     </div>
   );
 };
